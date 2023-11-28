955cc92ec6ce22ad4fdce339cbbbcdb225c7111b
bgomezv123 <bgomezv@unsa.edu.pe>
2023-11-28 15:45:54 -0500

    Juego de la vida en python v2

--- juego_de_la_vida.py
+++ juego_de_la_vida.py
@@ -15,51 +15,59 @@ pixel_size = 5
 
 tamanio = int(ventana_alto/pixel_size)
 
-def condicionVida(sum , tablero, i, j):
+def condicionVida(sum , _tablero, i, j):
     if  sum == 3 :
-        tablero[i][j] = 1
+        _tablero[i][j] = 1
     elif sum < 2 or sum >= 4:
-        tablero[i][j] = 0
-    else :
-        pass
+        _tablero[i][j] = 0
+
 
 def actualizar_tablero(tablero): 
+    tablero_aux =   [[0]*tamanio for _ in range(tamanio)]
+
+    for i in range(tamanio):
+        for j in range(tamanio):
+            tablero_aux[i][j] = tablero[i][j] 
+
     for i in range(tamanio):
         for j in range(tamanio):
             if i == 0: 
                 if j == 0: 
                     sum =  tablero[i][j+1] + tablero[i+1][j+1] + tablero[i+1][j] 
-                    condicionVida(sum, tablero, i, j)
+                    condicionVida(sum, tablero_aux, i, j)
 
                 elif j > 0 and j < (tamanio - 1) : 
                     sum =  tablero[i][j-1] + tablero[i+1][j-1] + tablero[i+1][j]+ tablero[i][j+1] + tablero[i+1][j+1] 
-                    condicionVida(sum, tablero, i, j)
+                    condicionVida(sum, tablero_aux, i, j)
 
                 else :
                     sum =  tablero[i][j-1] + tablero[i+1][j-1] + tablero[i+1][j] 
-                    condicionVida(sum, tablero, i, j)
+                    condicionVida(sum, tablero_aux, i, j)
 
             elif i > 0 and i < (tamanio -1) :
                 if j == 0: 
                     sum =  tablero[i-1][j] + tablero[i-1][j+1] + tablero[i][j+1]+ tablero[i+1][j] + tablero[i+1][j+1]  
-                    condicionVida(sum, tablero, i, j)
+                    condicionVida(sum, tablero_aux, i, j)
 
                 elif j > 0 and j < (tamanio - 1) : 
                     sum =  tablero[i-1][j-1] + tablero[i-1][j] + tablero[i-1][j+1]+ tablero[i][j-1] + tablero[i][j+1] +tablero[i+1][j-1] +tablero[i+1][j] +tablero[i+1][j+1] 
-                    condicionVida(sum, tablero, i, j)
+                    condicionVida(sum, tablero_aux, i, j)
                 else :
                     sum =  tablero[i-1][j] + tablero[i-1][j-1] + tablero[i][j-1]+ tablero[i+1][j-1] + tablero[i+1][j]  
-                    condicionVida(sum, tablero, i, j)
+                    condicionVida(sum, tablero_aux, i, j)
             else :
                 if j == 0:
                     sum =  tablero[i-1][j] + tablero[i-1][j+1] + tablero[i][j+1] 
-                    condicionVida(sum, tablero, i, j)  
+                    condicionVida(sum, tablero_aux, i, j)  
                 elif j > 0 and j < (tamanio - 1) :
                     sum =  tablero[i][j-1] + tablero[i-1][j-1] + tablero[i-1][j]+ tablero[i-1][j+1] + tablero[i][j+1] 
-                    condicionVida(sum, tablero, i, j)
+                    condicionVida(sum, tablero_aux, i, j)
                 else:
                     sum =  tablero[i][j-1] + tablero[i-1][j-1] + tablero[i-1][j] 
-                    condicionVida(sum, tablero, i, j)  
+                    condicionVida(sum, tablero_aux, i, j) 
+    return tablero_aux
+
+
 
 
 # Configurar la ventana
@@ -75,7 +83,12 @@ def dibujar_dibujo(surface, dibujo):
 
 # Bucle principal del juego
 
-tablero =  [[random.randint(0, 1) for _ in range(tamanio)] for _ in range(tamanio)]
+tablero_final =  [[0]*tamanio for _ in range(tamanio)]
+
+
+tablero_final[5][5] = 1
+tablero_final[5][6] = 1
+tablero_final[5][7] = 1
 
 
 
@@ -87,8 +100,9 @@ while True:
 
     # Dibujar el dibujo en la ventana
     ventana.fill((255, 255, 255)) 
-    actualizar_tablero(tablero) 
-    dibujar_dibujo(ventana, tablero)
+    dibujar_dibujo(ventana, tablero_final)
+    
+    tablero_final = actualizar_tablero(tablero_final)
 
     # Actualizar la pantalla
     pygame.display.flip()
